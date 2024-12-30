89fbb833788800e16ce603a3632c886ee5174634
Waysys <wshaffer@waysysweb.com>
2024-12-29 22:42:58 -0500

    Additional tests of Year

--- __tests__/year_test.js
+++ __tests__/year_test.js
@@ -13,19 +13,83 @@ import { Year } from "../year.js"
 
 describe('Year', () => {
 
-    test('isYear valid', () => {
-        const year = 1601;
+    test('isYear valid low', () => {
+        const year = Year.MinYear;
         const actualResult = Year.isYear(year);
         const expectedResult = true
         expect(actualResult).toEqual(expectedResult);
     });
 
-    test('isYear invalid', () => {
+    test('isYear valid high', () => {
+        const year = Year.MaxYear;
+        const actualResult = Year.isYear(year);
+        const expectedResult = true
+        expect(actualResult).toEqual(expectedResult);        
+    });
+
+    test('isYear invalid low', () => {
         const year = 1600;
         const actualResult = Year.isYear(year);
         const expectedResult = false
         expect(actualResult).toEqual(expectedResult);
-    })
+    });
+
+    test('isYear invalid high', () => {
+        const year = 4000;
+        const actualResult = Year.isYear(year);
+        const expectedResult = false
+        expect(actualResult).toEqual(expectedResult);
+    });
+
+    test('isLeapYear 2000', () => {
+        const year = 2000;
+        const actualResult = Year.isLeapYear(year);
+        const expectedResult = true
+        expect(actualResult).toEqual(expectedResult);       
+    });
+
+    test('isLeapYear 2000', () => {
+        const year = 1900;
+        const actualResult = Year.isLeapYear(year);
+        const expectedResult = false;
+        expect(actualResult).toEqual(expectedResult);       
+    });
+
+    test('isLeapYear 1900', () => {
+        const year = 1900;
+        const actualResult = Year.isLeapYear(year);
+        const expectedResult = false;
+        expect(actualResult).toEqual(expectedResult);       
+    });
+
+    test('isLeapYear 1904', () => {
+        const year = 1904;
+        const actualResult = Year.isLeapYear(year);
+        const expectedResult = true;
+        expect(actualResult).toEqual(expectedResult);       
+    });
+
+    test('isLeapYear 1903', () => {
+        const year = 1903;
+        const actualResult = Year.isLeapYear(year);
+        const expectedResult = false;
+        expect(actualResult).toEqual(expectedResult);       
+    });
+
+    test('daysInYear 1904', () => {
+        const year = 1904;
+        const actualResult = Year.daysInYear(year);
+        const expectedResult = 366;
+        expect(actualResult).toEqual(expectedResult);       
+    });
+
+    test('daysInYear 1900', () => {
+        const year = 1900;
+        const actualResult = Year.daysInYear(year);
+        const expectedResult = 365;
+        expect(actualResult).toEqual(expectedResult);       
+    });
+
 })
 
 // ----------------------------------------------------------------------------
